6d99655ed07b343619ee7625651b68ba0f9c1244
baekjunha <baekjunha00@gmail.com>
2026-09-03 15:10:56 +0900

    코드 추가

--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
 #include "Engine.h"
 
+#include <iostream>
+
+using namespace std;
+
+class Singleton
+{
+protected:
+	Singleton()
+	{
+	}
+
+public:
+	static Singleton* GetInstance()
+	{
+		if (!Singleton::Instance)
+		{
+			Singleton::Instance = new Singleton();
+		}
+
+		return Singleton::Instance;
+	}
+
+protected:
+	static Singleton* Instance;
+};
+
+Singleton* Singleton::Instance = nullptr;
+
+
+
+
 int main()
 {
-	UEngine* MyEngine = new UEngine();
+	//UEngine::GetInstance();
 
-	MyEngine->Init();
+	GEngine->Init();
+
+	GEngine->Run();
+
+	GEngine->Exit();
+
 
 	return 0;
 }
